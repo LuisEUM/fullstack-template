de597cd4186722a79259975133948810234e1383
¨LuisEUM¨ <luiseum95@gmail.com>
2024-01-27 23:49:43 +0100

    Eslint and Prettier fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import { Button } from "@/components/ui/button"
-import { buttonVariants } from "@/components/ui/button"
-import Link from "next/link"
+import { Button, buttonVariants } from '@/components/ui/button'
 
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      <Button variant="destructive" size={"lg"}>Button</Button>
-      <Link href={'https://www.google.es/'} className={buttonVariants({ variant: "outline" })}>Click here</Link>
+      <Button variant="destructive" size={'lg'}>
+        Button
+      </Button>
+      <Link href={'https://www.google.es/'} className={buttonVariants({ variant: 'outline' })}>
+        Click here
+      </Link>
     </main>
   )
 }
-
