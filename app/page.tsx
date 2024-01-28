1b5945c0ce4b77d2d3bd49dcdc7d5d22a29f5f54
ibbar <ibbar616@gmail.com>
2024-01-28 20:16:01 +0100

    cambie color de botones en Home

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main>
-      <Button variant="destructive" size={"lg"}>Button</Button>
-      <Link href={'https://www.google.es/'} className={buttonVariants({ variant: "outline" })}>Click here</Link>
+      <Button variant="default" size={"lg"}>Home</Button>
+      <Link href={'https://www.google.es/'} className={buttonVariants({ variant: "default", size: "lg"})}>Click here</Link>
     </main>
   )
 }
